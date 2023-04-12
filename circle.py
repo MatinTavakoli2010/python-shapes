d8ae4efe9b110211d44b4a2ef1997029b50d2028
MatinTavakoli2010 <130545024+MatinTavakoli2010@users.noreply.github.com>
2023-04-12 13:55:57 +0430

    Create circle.py

---
+++ circle.py
@@ -0,0 +1,12 @@
+import turtle
+laki=turtle.Turtle()
+laki.shape('turtle')
+laki.color('dark blue')
+laki.width(2)
+laki.speed(100)
+for j in range(12):
+    for i in range(20,101,10):
+             laki.circle(i)
+    laki.left(360/12)
+laki.hideturtle()
+#Matin tavakoli
