f7ffe12116f43368e92dbbfe2c9151ffa5041ad7
MatinTavakoli2010 <130545024+MatinTavakoli2010@users.noreply.github.com>
2023-04-12 17:25:37 +0430

    Create hexagon pro.py

---
+++ hexagon pro.py
@@ -0,0 +1,14 @@
+import turtle
+laki=turtle.Turtle()
+laki.shape('turtle')
+laki.color('dark blue')
+laki.width(0.5)
+laki.speed(100)
+for j in range(8):
+    for x in range(10,141,5):
+        for i in range(6):
+            laki.forward(x)
+            laki.right(360/6)
+        laki.right(45)
+laki.hideturtle()
+#Matin tavakoli
