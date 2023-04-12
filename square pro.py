6418fb27c59dadd13d3425d44f40122c71cd94b1
MatinTavakoli2010 <130545024+MatinTavakoli2010@users.noreply.github.com>
2023-04-12 17:15:55 +0430

    Create square pro.py

---
+++ square pro.py
@@ -0,0 +1,14 @@
+import turtle
+laki=turtle.Turtle()
+laki.shape('turtle')
+laki.color('dark blue')
+laki.width(0.5)
+laki.speed(100)
+for j in range(8):
+    for x in range(20,151,5):
+        for i in range(4):
+            laki.forward(x)
+            laki.right(90)
+        laki.right(45)
+laki.hideturtle()
+#Matin tavakoli
