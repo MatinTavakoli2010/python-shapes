535e16a61baf063c1f980ae111f47ca5adf889e3
MatinTavakoli2010 <130545024+MatinTavakoli2010@users.noreply.github.com>
2023-04-12 19:08:32 +0430

    Create Square2.py

---
+++ Square2.py
@@ -0,0 +1,11 @@
+import turtle
+laki=turtle.Turtle()
+laki.color('dark blue')
+laki.width(2)
+laki.speed(5)
+for x in range(20,151,30):
+    for i in range(4):
+        laki.forward(x)
+        laki.left(90)
+laki.screen.exitonclick()
+#Matin tavakoli
